3cd547f3adf1bce253add5ad6188c05e966d4b7b
Siverslayer <164073129+Siverslayer@users.noreply.github.com>
2024-05-19 00:26:16 +0300

    Add files via upload

---
+++ cpu_info.py
@@ -0,0 +1,12 @@
+import time
+import cpuinfo
+
+def get_cpu_info():
+    info = cpuinfo.get_cpu_info()
+    return info
+
+if __name__ == "__main__":
+          
+            cpu_info = get_cpu_info()
+            print(f"CPU Info: {cpu_info['brand_raw']}, {cpu_info['hz_actual_friendly']}")
+            time.sleep(1)
